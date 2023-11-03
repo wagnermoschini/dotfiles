8715fbdea3a05cc4509d0a8e6fc2b7f9ff87e71f
Wagner Moschini <wagnermoschini@Wagners-MacBook-Pro-2.local>
2023-11-03 13:36:30 -0300

    update brew installation

--- init.sh
+++ init.sh
@@ -9,7 +9,7 @@ git config --global core.autocrlf input
 # Check for homebrew and install it
 if test ! $(which brew); then
   echo "Installing homebrew"
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 else
   echo "homebrew allready instaled"
 fi
@@ -18,38 +18,38 @@ fi
 brew update
 
 # Install dependencies (Brewfile)
-brew tap homebrew/bundle
-brew bundle
+#brew tap homebrew/bundle
+#brew bundle
 
 
-echo "Beginning node stuffs"
-# Create NVM dir
-mkdir ~/.nvm
-echo ".nvm dir created"
+#echo "Beginning node stuffs"
+## Create NVM dir
+#mkdir ~/.nvm
+#echo ".nvm dir created"
 
-export NVM_DIR="$HOME/.nvm"
-  . "/usr/local/opt/nvm/nvm.sh"
+#export NVM_DIR="$HOME/.nvm"
+#  . "/usr/local/opt/nvm/nvm.sh"
 
-nvm install node
-echo "last node version instaled"
-echo "node stuffs done"
+#nvm install node
+#echo "last node version instaled"
+#echo "node stuffs done"
 
 # Install oh-my-zsh
-curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
+#curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
 
 # link .zshrc
-ln -sf ~/dotfiles/.zshrc ~/.zshrc
+#ln -sf ~/dotfiles/.zshrc ~/.zshrc
 
 # Make ZSH default
-chsh -s $(which zsh)
+#chsh -s $(which zsh)
 
 # Install z
-echo "Installing z"
-git clone https://github.com/rupa/z.git ~/z
+#echo "Installing z"
+#git clone https://github.com/rupa/z.git ~/z
 
-sh ./vim/init.sh
+#sh ./vim/init.sh
 
 # TMUX CONF
 # link .tmux.conf
-echo "tmux conf link"
-ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+#echo "tmux conf link"
+#ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
